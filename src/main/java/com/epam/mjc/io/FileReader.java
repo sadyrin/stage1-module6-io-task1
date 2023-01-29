9f67ea07e188d643eb2e8619d0af3e2f1df49e26
sadyrin <sadyrin@gmail.com>
2023-01-29 16:15:07 +0200

    done task stage1-module6-io-task1

--- src/main/java/com/epam/mjc/io/FileReader.java
+++ src/main/java/com/epam/mjc/io/FileReader.java
@@ -2,10 +2,35 @@ package com.epam.mjc.io;
 
 import java.io.File;
 
+import java.io.BufferedReader;
+
 
 public class FileReader {
+    private String name;
+    private Integer age;
+    private String email;
+    private Long phone;
 
     public Profile getDataFromFile(File file) {
-        return new Profile();
+		
+		try(java.io.FileReader reader = new java.io.FileReader(file);){
+			
+			BufferedReader br = new BufferedReader(reader);
+
+			String line = null;
+
+			while ((line = br.readLine()) != null) {
+				if(line.contains("Name:")) name = line.split(": ")[1];
+				if(line.contains("Age:")) age = Integer.parseInt(line.split(": ")[1]);
+				if(line.contains("Email:")) email = line.split(": ")[1];
+				if(line.contains("Phone:")) phone = Long.parseLong(line.split(": ")[1]);
+			}
+
+			br.close();
+		} catch (Exception e1) {
+			e1.printStackTrace();
+		} 
+
+        return new Profile(name, age, email, phone);
     }
 }
